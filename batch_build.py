41f919e9a99c6c215b200b531789354cc1878bde
Joshua Z. Zhang <cheungchih@gmail.com>
2020-05-01 14:30:08 -0700

    update

--- batch_build.py
+++ batch_build.py
@@ -7,8 +7,8 @@ import gluoncv as gcv
 def build(app):
     try:
         print('Building for {}...'.format(app))
-        subprocess.check_output(['cd container', '&&', './build_and_push.sh',
-                                 'gluoncv_{}'.format(app), app], shell=True)
+        subprocess.check_output(['cd container && ./build_and_push.sh gluoncv_{} {}'.format(app, app)],
+                                 shell=True)
     except subprocess.CalledProcessError as e:
         out_bytes = e.output       # Output generated before error
         code      = e.returncode   # Return code
